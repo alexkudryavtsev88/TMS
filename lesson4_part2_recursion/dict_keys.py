79c2781972a6f96867b733e3765a4a4b05a73d10
alexkudryavtsev88 <aliaksei.kudrautsau@gmail.com>
2023-05-30 23:01:14 +0300

    lesson5

--- lesson4_part2_recursion/dict_keys.py
+++ lesson4_part2_recursion/dict_keys.py
@@ -1,23 +1,55 @@
+from typing import Callable, Any
+
 CACHE = {}
 
 
-def get_user_data(name, surname, age):
-    default_message = """
-    user_name={0}, user_surname={1}, user_age={2}
-    """
-    key = build_key(name, surname, age)
+def get_user_data(
+    build_key_func: Callable[[str, str, int], Any],
+    name: str,
+    surname: str,
+    age: int,
+):
+    key = build_key_func(name, surname, age)
     if (value := CACHE.get(key)) is not None:
         return value
 
-    user_message = default_message.format(
-        name, surname, age
-    )
-    CACHE[key] = user_message
-    return user_message
+    user_data = {
+        'name': name,
+        'surname': surname,
+        'age': age
+    }
+    CACHE[key] = user_data
+    return user_data
 
 
-def build_key(name, surname, age):
+def build_key_tuple_with_immutables(
+    name: str,
+    surname: str,
+    age: int
+) -> tuple[str, str, tuple[int]]:
+    return name, surname, (age, )
+
+
+def build_key_tuple_with_mutables(
+    name: str,
+    surname: str,
+    age: int
+) -> tuple[str, str, list[int]]:
     return name, surname, [age]
+
+def build_key_set(
+    name: str,
+    surname: str,
+    age: int
+) -> set[str, str, int]:
+    return {name, surname, age}
+
+def build_key_list(
+    name: str,
+    surname: str,
+    age: int
+) -> list[str, str, int]:
+    return [name, surname, age]
 
 
 # TEST
@@ -25,12 +57,24 @@ user_name = "Alex"
 user_surname = "Kudryavtsev"
 user_age = 34
 
-user_data = get_user_data(
-    user_name,
-    user_surname,
-    user_age
-)
-print(user_data)
+for func in (
+    build_key_list,
+    build_key_set,
+    build_key_tuple_with_mutables,
+    build_key_tuple_with_immutables
+):
+    try:
+        user_data = get_user_data(
+            func,
+            user_name,
+            user_surname,
+            user_age,
+        )
+    except TypeError as exc:
+        print(exc)
+    else:
+        print(user_data)
+
 
 
 class MyClass:
