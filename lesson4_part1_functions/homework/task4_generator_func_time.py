e61d8653721369ea3074360b9c849294b4a25223
alexkudryavtsev88 <aliaksei.kudrautsau@gmail.com>
2023-05-30 18:46:12 +0300

    lesson4_homework

---
+++ lesson4_part1_functions/homework/task4_generator_func_time.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from time import sleep
+
+
+def get_my_time(
+    seconds: int = 1,
+    time_format: str = '%Y-%m-%d %H:%M:%S'
+) -> str:
+    sleep(seconds)
+    return datetime.strftime(datetime.now(), time_format)
+
+
+n = int(input('Введите целое число:\n'))
+time_list = [get_my_time() for _ in range(n)]
+print(f'{time_list=}')
