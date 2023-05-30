43ed9da691a11bde93ca4e99b44e22012704f6d7
alexkudryavtsev88 <aliaksei.kudrautsau@gmail.com>
2023-05-30 18:34:14 +0300

    lesson4_homework

---
+++ lesson4_part1_functions/homework/task3_counter.py
@@ -0,0 +1,22 @@
+from typing import Collection
+from collections import Counter
+
+
+def count_elements(collection):
+    duplicates = {}
+    for item in collection:
+        if item in duplicates:
+            duplicates[item] += 1
+        else:
+            duplicates[item] = 1
+
+    return dict(sorted(duplicates.items(), key=lambda x: x[1], reverse=True))
+
+
+input_list = [1, 2, 1, 3, 4, 5, 3, 6, 7, 3, 8, 9, 4]
+counter_my = count_elements(input_list)
+counter_lib = Counter(input_list)
+
+print(counter_my)
+print(counter_lib)
+
