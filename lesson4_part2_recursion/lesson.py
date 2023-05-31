4894ff3e7fa07925ee70d4415800e0e4de3f86a8
alexkudryavtsev88 <aliaksei.kudrautsau@gmail.com>
2023-05-31 09:39:44 +0300

    Updates

--- lesson4_part2_recursion/lesson.py
+++ lesson4_part2_recursion/lesson.py
@@ -1,15 +1,14 @@
 from collections import deque
-import queue
 
 x = [1, 2, 3, 4, 5]
 dq = deque(x)
 print(dq)
 
-dq.append(6)
+dq.append(6)  # Добавление элемента в КОНЕЦ за Константное время
 print(dq)
-dq.pop()
+dq.pop()      # Удаление элемента с КОНЦА за Константное время
 print(dq)
-dq.appendleft(10)
+dq.appendleft(10)  # Добавление элемента в НАЧАЛО за Константное время
 print(dq)
-dq.popleft()
+dq.popleft()   # Удаление элемента из НАЧАЛА за Константное время
 print(dq)
