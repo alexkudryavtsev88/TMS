3203bff1a0144462f6bdbfc641d1280f953cd0f9
alexkudryavtsev88 <aliaksei.kudrautsau@gmail.com>
2023-05-31 09:41:31 +0300

    Updates

--- lesson4_part2_recursion/lesson_example_recursive_simple.py
+++ lesson4_part2_recursion/lesson_example_recursive_simple.py
@@ -3,6 +3,7 @@
 в котором значения ключей могут быть ТОЛЬКО строками или вложенными словарями
 """
 
+
 def recursive_simple(src: dict, lookup_value: str):
     for key, current_value in src.items():
         if isinstance(current_value, dict):
@@ -11,10 +12,10 @@ def recursive_simple(src: dict, lookup_value: str):
             # 'src' переменную v, которая имеет тип dict
         else:
             if current_value == lookup_value:
-                print(f"{lookup_value} is found!")
                 return current_value
 
-src = {
+
+source_dict = {
     'key1': 'John',
     'key2': {
         'key3': 'Bob',
@@ -28,9 +29,8 @@ src = {
         },
     }
 }
-found_name = recursive_simple(src, 'Sam')  # напечатает 'Alex is found!'
-print(found_name)
-# recursive_simple(src, 'Bob')   # напечатает 'Alex is found!'
-# recursive_simple(src, 'Jessica')  # ничего не напечатает, так как слова 'Jessica' нет ни на одном уровне вложенности
+print(recursive_simple(source_dict, 'Alex'))  # напечатает 'Alex is found!'
+print(recursive_simple(source_dict, 'Bob'))   # напечатает 'Alex is found!'
+print(recursive_simple(source_dict, 'Jessica'))  # напечатает None, так как слова 'Jessica' нет ни на одном уровне вложенности
 
 
