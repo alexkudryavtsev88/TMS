a27a18e566c45057cbe2967230f063385daedf1f
alexkudryavtsev88 <aliaksei.kudrautsau@gmail.com>
2023-05-31 09:37:47 +0300

    Updates

--- lesson4_part2_recursion/dict_keys.py
+++ lesson4_part2_recursion/dict_keys.py
@@ -9,6 +9,10 @@ def get_user_data(
     surname: str,
     age: int,
 ):
+    """
+    Простая реализация алгоритма получения данных с использованием кэширования
+    В качестве кэша мы будем использовать обычный dict
+    """
     key = build_key_func(name, surname, age)
     if (value := CACHE.get(key)) is not None:
         return value
@@ -26,30 +30,55 @@ def build_key_tuple_with_immutables(
     name: str,
     surname: str,
     age: int
-) -> tuple[str, str, tuple[int]]:
-    return name, surname, (age, )
+):
+    """
+    Кортеж - НЕизменяемый тип данных.
+    Но объект кортежа может быть КЛЮЧОМ в Словаре
+    тогда и только тогда, когда он содержит
+    в себе объекты НЕизменяемых типов: int, float, bool, str
+    или других Кортежей, содержащих НЕизменяемые типы
+    """
+    return name, (surname, (age, ))
 
 
 def build_key_tuple_with_mutables(
     name: str,
     surname: str,
     age: int
-) -> tuple[str, str, list[int]]:
+):
+    """
+    В данном примере Кортеж НЕ может быть
+    КЛЮЧОМ словаря, так как он содержит внутри себя
+    объект типа list, который является Изменяемым
+    """
     return name, surname, [age]
+
 
 def build_key_set(
     name: str,
     surname: str,
     age: int
-) -> set[str, str, int]:
+):
+    """set - Именяемый объект и НЕ может быть КЛЮЧОМ словаря"""
     return {name, surname, age}
+
 
 def build_key_list(
     name: str,
     surname: str,
     age: int
-) -> list[str, str, int]:
+):
+    """list - Именяемый объект и НЕ может быть КЛЮЧОМ словаря"""
     return [name, surname, age]
+
+
+def build_key_dict(
+    name: str,
+    surname: str,
+    age: int
+):
+    """dict - Именяемый объект и НЕ может быть КЛЮЧОМ другого словаря"""
+    return {name: name, surname: surname, age: age}
 
 
 # TEST
@@ -60,6 +89,7 @@ user_age = 34
 for func in (
     build_key_list,
     build_key_set,
+    build_key_dict,
     build_key_tuple_with_mutables,
     build_key_tuple_with_immutables
 ):
@@ -71,18 +101,9 @@ for func in (
             user_age,
         )
     except TypeError as exc:
-        print(exc)
+        print(f"ERROR in {func.__qualname__}: {exc}")
     else:
-        print(user_data)
+        print(f"SUCCESS in {func.__qualname__}: value = {user_data}")
 
-
-
-class MyClass:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def __str__(self):
-        return f'Name={self.name}, Age={self.age}'
 
 
