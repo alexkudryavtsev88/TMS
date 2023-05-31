77dc73c6c3a7e383fcfd4391707ebae3ad8ee335
alexkudryavtsev88 <aliaksei.kudrautsau@gmail.com>
2023-05-31 09:43:39 +0300

    Updates

--- lesson4_part2_recursion/homework_recursion/homework.py
+++ lesson4_part2_recursion/homework_recursion/homework.py
@@ -24,28 +24,63 @@
 * понятие "уровень вложенности" или "глубина" касается только вложенных словарей!
 """
 
-from homework_recursion.source.source_dict import get_source_dict_with_duplicates
 
-"""Task 4: recursive search in dict"""
+def get_source_dict():
+    return {
+        "key1": "John",  # deep 0
+        'key2': {
+            'key3': 'Alex',  # deep 1
+            'key4': {
+                'key5': ['Kate', 'Mary'],  # deep 2
+                'key6': {
+                    'key7': [
+                        'Bob',  # deep 3
+                        'Duke',
+                        {
+                            'key8': {  # deep 4
+                                'key9': [  # deep 5
+                                    'Lisa',
+                                    {
+                                        'key10': ['Mark']  # deep 6
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                },
+            },
+            'key8': 'Robert'  # deep 1
+        }
+    }
 
 
-def recursive_search(src: dict, value: str, deep=-1, parent=None):
-    if isinstance(src, dict):
-        deep += 1
-        for k, v in src.items():
-            recursive_search(v, value, deep=deep, parent=k)
-    elif isinstance(src, list):
-        for item in src:
-            recursive_search(item, value, deep=deep, parent=parent)
-    elif isinstance(src, str):
-        if src == value:
-            print(f'Found "{value}" on deep = {deep}, parent = {parent}')
-            return src
+""" Пример словаря c дубликатами: """
 
 
-""" TEST """
-lookup_name = "Alex"
-source_dict = get_source_dict_with_duplicates()
-recursive_search(source_dict, lookup_name)
-
-
+def get_source_dict_with_duplicates():
+    return {
+        "key1": {
+            "key2": {
+                "key3": [
+                    "John",
+                    {
+                        "key4": "Bob",
+                        "key5": "Alex",
+                        "key6": {
+                            "key7": [
+                                {
+                                    "key7": "Jessica",
+                                    "key8": {
+                                        "key9": [
+                                            "Alex"
+                                        ]
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        },
+        "key4": "Kate"
+    }
