33998c3ed273f98082ea2e4e7fe61891a49eb5e3
alexkudryavtsev88 <aliaksei.kudrautsau@gmail.com>
2023-06-02 19:07:22 +0300

    Updates

--- lesson4_part2_recursion/homework_recursion/solutions/task4_recursive_search.py
+++ lesson4_part2_recursion/homework_recursion/solutions/task4_recursive_search.py
@@ -1,51 +1,62 @@
 """
-Дан входной словарь
-- Ключи словаря - строки
-- Значениями ключей могут быть:
--- Строки
--- Словари
--- Списки
+Изменить реализацию функции рекурсивного поиска элемента в словаре (из предыдущего задания)
+следующим образом:
+- функция должна находить ПЕРВОЕ соответствие имени и возвращать результат в виде словаря:
 
-Все элементы-словари, как и родительский словарь, могут содержать строки, списки и другие вложенные словари
-Все Элементы-списки могут содержать строки, словари и другие вложенные списки
-
-Написать рекурсивную функцию, которая:
-- Принимает на вход следующие аргументы:
--- 1 аргумент: исходный словарь
--- 2 аргумент: слово, которое нужно найти в словаре (строка)
--- аргумент deep с дефолтным целочисленным значением, отображающим текущий уровень вложенности (глубину) словаря
--- аргумент parent с дефолтным значением None, который сохраняет родителя (ключ словаря) на текущем уровне вложенности
-
-- Делает обход словаря в глубину и поиск указанного значения
-- Если значение найдено: печатает строку f"Значение {val} найдено на глубине {deep}"
-
-
-* будет ли ваша функция искать слово до первого совпадения или найдет все имеющиеся совпадения - не важно
-* понятие "уровень вложенности" или "глубина" касается только вложенных словарей!
+    {'val': found_value, 'parent': found_value_parent, 'deep': found_value_deep}
 """
 
-from homework_recursion.source.source_dict import get_source_dict_with_duplicates
+from homework_recursion.source import source_dict
 
-"""Task 4: recursive search in dict"""
+""" Task 4: recursive search"""
+def recursive_search(source: dict, lookup_value: str, deep=-1, parent=None):
+    result = None
 
+    def _recursive_inner(src: dict, lookup: str, deep_=-1, parent_=None):
+        nonlocal result
 
-def recursive_search(src: dict, value: str, deep=-1, parent=None):
-    if isinstance(src, dict):
-        deep += 1
-        for k, v in src.items():
-            recursive_search(v, value, deep=deep, parent=k)
-    elif isinstance(src, list):
-        for item in src:
-            recursive_search(item, value, deep=deep, parent=parent)
-    elif isinstance(src, str):
-        if src == value:
-            print(f'Found "{value}" on deep = {deep}, parent = {parent}')
-            return src
+        if isinstance(src, dict):
+            deep_ += 1
+            for key, val in src.items():
+                if result := _recursive_inner(
+                    val, lookup, deep_, key
+                ):
+                    return result
+        elif isinstance(src, (list, tuple, set)):
+            for item in src:
+                if result := _recursive_inner(
+                    item, lookup, deep_, parent_
+                ):
+                    return result
+        elif isinstance(src, str) and src == lookup:
+            return {'val': lookup, 'parent': parent_, 'deep': deep_}
+
+    return _recursive_inner(source, lookup_value, deep, parent)
 
 
 """ TEST """
-lookup_name = "Alex"
-source_dict = get_source_dict_with_duplicates()
-recursive_search(source_dict, lookup_name)
+
+"""1. Find all expected names"""
+_dict = source_dict.get_source_dict()
+values = [
+    ("John", {'val': 'John', 'parent': 'key1', 'deep': 0}),
+    ("Ann", {'val': 'Ann', 'parent': 'key3', 'deep': 1}),
+    ('Kate', {'val': 'Kate', 'parent': 'key5', 'deep': 2}),
+    ('Mary', {'val': 'Mary', 'parent': 'key5', 'deep': 2}),
+    ('Bob', {'val': 'Bob', 'parent': 'key7', 'deep': 3}),
+    ('Duke', {'val': 'Duke', 'parent': 'key7', 'deep': 3}),
+    ('Lisa', {'val': 'Lisa', 'parent': 'key9', 'deep': 5}),
+    ("Mark", {'val': 'Mark', 'parent': 'key10', 'deep': 6}),
+    ("Alex", {'val': 'Alex', 'parent': 'key10', 'deep': 6}),  # have Duplicate!
+    ('Louisa', {'val': 'Louisa', 'parent': 'key11', 'deep': 5}),
+    ('Robert', {'val': 'Robert', 'parent': 'key12', 'deep': 1}),
+    ('Ronaldo', {'val': 'Ronaldo', 'parent': 'key13', 'deep': 0})
+]
+for lookup_value, expected_result in values:
+    result = recursive_search(_dict, lookup_value)
+    print(result)
+    assert result == expected_result, f"{result} != {expected_result}"
+
+
 
 
