145d66961cf4f790931891d9dacd7f9bcf8bb5f0
alexkudryavtsev88 <aliaksei.kudrautsau@gmail.com>
2023-06-16 22:12:01 +0300

    lesson9

--- lesson8_9_10_OOP/part_2/part4_inner_dicts.py
+++ lesson8_9_10_OOP/part_2/part4_inner_dicts.py
@@ -27,3 +27,28 @@ for k, v in my_obj.__class__.__dict__.items():
     print(f"{k} -> {v}")
 
 
+class ClassWithoutDict:
+
+    __slots__ = ("name", "age")  # строго задаем какие атрибуты может содержать наш будущий инстанс класса
+
+    def __init__(self):
+        self.name = "Alex"
+        self.age = 34
+
+    def __str__(self):
+        return f"{self.name}, {self.age}"
+
+
+x = ClassWithoutDict()
+print(x)
+try:
+    print(x.__dict__)  # инстанс, у которого объявлены __slots__ НЕ ИМЕЕТ внутреннего дикта!
+except AttributeError as exc:
+    print(exc)
+
+try:
+    x.test = "123"  # нельзя присвоить НОВЫЙ атрибут
+except AttributeError as exc:
+    print(exc)
+
+
