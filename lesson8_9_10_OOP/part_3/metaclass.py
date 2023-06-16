a96697fc65f09553fd90e1b71b1a592e9031517b
alexkudryavtsev88 <aliaksei.kudrautsau@gmail.com>
2023-06-16 22:08:51 +0300

    lesson9

--- lesson8_9_10_OOP/part_3/metaclass.py
+++ lesson8_9_10_OOP/part_3/metaclass.py
@@ -29,3 +29,30 @@ print(int2, type(int2))
 my_obj = type('MyNewClass', (Integer, ), {'x': 1, 'y': 2})
 print(my_obj)
 print(type(my_obj))
+
+# ------------------------------------------------------ #
+
+
+class Parent1:
+    x = "some_x"
+
+
+class Parent2:
+    y = "some_y"
+
+
+# Создание нового типа (класса) в Рантайме с использованием функции type
+new_type = type("MyNewType", (Parent1, Parent2), {"attr": "some_attr"})
+# где:
+# - первый аргумент - имя создаваемого типа
+# - второй аргумент - кортеж "родителей" для создаваемого типа
+# - третий аргумент - дикт с атрибутами создаваемого типа
+print(new_type)
+print(new_type.attr)
+print(new_type.__dict__["attr"])
+
+instance = new_type()
+print(instance)
+instance.attr = "some_instance_attr"
+assert instance.attr == "some_instance_attr"
+assert instance.__class__.attr == "some_attr"
