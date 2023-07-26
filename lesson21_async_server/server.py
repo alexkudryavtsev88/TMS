78daa38cbec20175e7ca338e4c27d1ee9309e856
alexkudryavtsev88 <aliaksei.kudrautsau@gmail.com>
2023-07-26 14:16:16 +0300

    SQLAlchemy

---
+++ lesson21_async_server/server.py
@@ -0,0 +1,31 @@
+class Server:
+
+    # def __init__(self, name: str, age: int, gender: str, nationality: str = "belarus"):
+    #     self._name = name
+    #     self._age = age
+    #     self._gender = gender
+    #     self._nationality = nationality
+
+    async def add_post(self):
+        pass
+
+    async def add_comment(self, post_title: str):
+        pass
+
+    async def add_like(self, post_title: str):
+        pass
+
+    async def edit_post(self, post_title: str, edit_data: dict[str, str]):
+        pass
+
+    async def edit_comment(self, comment_title: str):
+        pass
+
+    def delete_post(self, post_title: str):
+        pass
+
+    def delete_comment(self, comment_title: str):
+        pass
+
+    def delete_like(self, post_title: str):
+        pass
