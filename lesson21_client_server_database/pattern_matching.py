8a825d4076ce2dc71871f61acca0fed18bd24bd9
alexkudryavtsev88 <aliaksei.kudrautsau@gmail.com>
2023-07-27 22:35:43 +0300

    sqlalchemy

--- lesson21_client_server_database/pattern_matching.py
+++ lesson21_client_server_database/pattern_matching.py
@@ -1,27 +1,28 @@
-def get_value(key: int) -> tuple[str, ...] | None:
-    my_dict: dict[int, tuple[str, ...]] = {
-        1: ("test", ),
-        2: ("Alex", "Ann"),
-        3: ("0", "1", "2", "3", "4"),
-        4: (),
-    }
-    return my_dict.get(key)
 
-
-def match_result(result):
+def match_result(dict_: dict[int, tuple[str, ...]], key: int) -> str:
+    result = dict_.get(key)
     match result:
         case None:
-            print("value not found!")
+            return "Case 1 (None): value not found!"
         case name_1, name_2:
-            print(f"Two names: {name_1}, {name_2}")
+            return f"Case 2 (Two names): {name_1}, {name_2}"
         case "0", *_:
-            print([int(n) for n in result])
+            return f"Case 3 (Numbers started from 0): {[int(n) for n in result]}"
         case _, "1" | "2", *_:
-            print([int(n) for n in result])
+            return f"Case 4 (Numbers, second number is 1 OR 2): {[int(n) for n in result]}"
+        case element, :
+            return f"Case 5 (Tuple with only one element): {element}"
         case _:
-            print("I don't know what to do!")
+            return "Case Unknown: I don't know that to do!"
 
 
-for i in range(5):
-    result = get_value(i)
-    match_result(result)
+my_dict = {
+    1: ("test", ),
+    2: ("Alex", "Ann"),
+    3: ("0", "1", "2", "3", "4"),
+    4: ("-1", "1", "2", "3", "4"),
+    5: (),
+}
+for i in range(6):
+    result = match_result(my_dict, i)
+    print(f"key: {i}, result: {result}")
