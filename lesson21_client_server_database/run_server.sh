90b3cae533d3efe770b895b96ba0289c8ebd2b6d
alexkudryavtsev88 <aliaksei.kudrautsau@gmail.com>
2023-07-28 16:55:58 +0300

    server + SQLAlchemy

--- lesson21_client_server_database/run_server.sh
+++ lesson21_client_server_database/run_server.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-export PYTHONPATH=/home/a2kudryavtsev/tms/students/1_aliaksei_kudrautsau/TMS
 
-python /home/a2kudryavtsev/tms/students/1_aliaksei_kudrautsau/TMS/lesson21_client_server_database/client_and_server/server.py
+# Copy the PATH to the your ROOT folder (where all this code is placed) and set this PATH
+# instead of my path!
+YOUR_ROOT_PATH=/home/a2kudryavtsev/tms/students/1_aliaksei_kudrautsau/TMS
+
+export PYTHONPATH=${YOUR_ROOT_PATH}
+
+python ${YOUR_ROOT_PATH}/lesson21_client_server_database/client_and_server/server.py
