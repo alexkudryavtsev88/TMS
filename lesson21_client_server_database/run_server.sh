c8639780b8d4bd2bf6dcdf605e6165df0731238e
alexkudryavtsev88 <aliaksei.kudrautsau@gmail.com>
2023-07-29 12:41:47 +0300

    server + SQLAlchemy

--- lesson21_client_server_database/run_server.sh
+++ lesson21_client_server_database/run_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-# Copy the PATH to the your ROOT folder (where all this code is placed) and set this PATH
+# Copy the PATH to your ROOT folder (where all this code is placed) and set this PATH
 # instead of my path!
 YOUR_ROOT_PATH=/home/a2kudryavtsev/tms/students/1_aliaksei_kudrautsau/TMS
 
