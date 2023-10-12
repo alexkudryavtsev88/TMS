60b93d7f8da5d4914b2f9f7a1d0c7703cdba382c
Aleksey Kudryavtsev <external.aleksey.kudryavtse@distribusion.com>
2023-10-12 23:48:05 +0300

    Aiohttp Websockets

--- lesson41_websockets/client.py
+++ lesson41_websockets/client.py
@@ -16,21 +16,19 @@ async def prompt_and_send(ws):
 
 
 async def main():
-    session = aiohttp.ClientSession()
     msg_types = {aiohttp.WSMsgType.CLOSED, aiohttp.WSMsgType.ERROR}
 
-    async with session.ws_connect(_WS_ENDPOINT) as ws:
-        await prompt_and_send(ws)
-        async for msg in ws:
-            print('Message received from server:', msg)
+    async with aiohttp.ClientSession() as session:
+        async with session.ws_connect(_WS_ENDPOINT) as ws:
             await prompt_and_send(ws)
+            async for msg in ws:
+                print('Message received from server:', msg)
+                await prompt_and_send(ws)
 
-            if msg.type in msg_types:
-                break
+                if msg.type in msg_types:
+                    break
 
 
 if __name__ == '__main__':
     print('Type "exit" to quit')
-    # loop = asyncio.get_event_loop()
-    # loop.run_until_complete(main())
     asyncio.run(main())
